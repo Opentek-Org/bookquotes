5003c1b8b4fe9ab0fdb933ccfbeb5908201c50e6
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-09-30 21:22:08 +0530

    Make function capable for different types

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let array = require("./data/data.json");
+let inspireArray = require("./data/inspire.json");
 
 let favline = {};
 
@@ -8,13 +9,28 @@ function randomInt(min, max) {
 
 module.exports.getRandomLines = function () {
   let index = randomInt(0, array.length);
-  return array[index].line;
+   switch(type){
+    case 'isp' : 
+        return inspireArray[index].line;
+    default:
+        return array[index].line;    
+  }
 };
 
 module.exports.getLines = function () {
-  let index = randomInt(0, array.length);
-  favline.line = array[index].line;
-  favline.book = array[index].book;
-  favline.author = array[index].author;
-  return favline;
+  let index = randomInt(0, array.length-1);
+  switch(type){
+    case "isp":
+        console.log(index);
+        favline.line = inspireArray[index].line;
+        favline.book = inspireArray[index].book;
+        favline.author = inspireArray[index].author;
+        return favline;
+    default:
+        console.log(index);
+        favline.line = array[index].line;
+        favline.book = array[index].book;
+        favline.author = array[index].author;    
+        return favline;
+  }
 };
