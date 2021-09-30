771b9a2fb50dc56c4301091fe98109b46685c20a
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-09-30 21:23:03 +0530

    Different type test added.

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const favline = require("./index.js");
 
-var myLine = favline.getLines();
+let myLine = favline.getLines();
+let inspireLine = favline.getLines('isp');
 
 console.log("*********************************");
 console.log("WE ARE GRAMMAR_NAZIS\n");
@@ -9,5 +10,10 @@ console.log("*********************************");
 console.log("The lines I love- " + myLine.line);
 console.log("Written by - " + myLine.author);
 console.log("From the book - " + myLine.book);
+
+console.log("The lines which motivates me- " + inspireLine.line);
+console.log("Written by - " + inspireLine.author);
+console.log("From the book - " + inspireLine.book);
+
 
 
