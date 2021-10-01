74a49148a0e4740365f53f1ff78d32125f6e3901
DADDY-DOUBLESHOT <prathams1001@gmail.com>
2021-10-01 22:56:41 +0530

    Added few nice book quotes and a hash map function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let array = require("./data/data.json");
+let viewed =[];  //hashtable 
 
 let favline = {};
 
@@ -8,8 +9,25 @@ function randomInt(min, max) {
 
 module.exports.getRandomLine = function () {
   let index = randomInt(0, array.length);
+  viewed[index]=1;
   return array[index].line;
 };
+
+
+
+module.exports.getNewRandomLine = function () {   //new function to know which line was prevoiulsy generated and to not generate it again this time
+
+  let index = randomInt(0, array.length);
+  let cnt=Number(array.length);
+  while(!viewed.includes(index)&&cnt>=0)  //might get executed in  1 iteration or might take log(n) iterations if used more in a day
+  {
+      index =randomInt(0, array.length);
+      cnt--;
+  }
+  return array[index].line;
+
+}
+
 
 module.exports.getLines = function () {
   let index = randomInt(0, array.length);
