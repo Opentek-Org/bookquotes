8a8ff3344db863665db67505fbd1a9f47367ff77
DADDY-DOUBLESHOT <prathams1001@gmail.com>
2021-10-02 13:44:07 +0530

    hashmap with O(1) search time added

--- index.js
+++ index.js
@@ -4,31 +4,32 @@ let viewed = new Array(array.length); //hashtable
 let favline = {};
 
 function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-module.exports.getRandomLine = function() {
-    let index = randomInt(0, array.length);
-    viewed[index] = 1;
-    return array[index].line;
+module.exports.getRandomLine = function () {
+  let index = randomInt(0, array.length);
+  viewed[index] = 1;
+  return array[index].line;
 };
 
-module.exports.getNewRandomLine = function() {
-    //new function to know which line was prevoiulsy generated and to not generate it again this time
+module.exports.getNewRandomLine = function () {
+  //new function to know which line was prevoiulsy generated and to not generate it again this time
 
-    let index = randomInt(0, array.length);
-    let cnt = Number(array.length);
-    while (!viewed[index] && cnt >= 0) { //O(1) search 
-        index = randomInt(0, array.length);
-        cnt--;
-    }
-    return array[index].line;
+  let index = randomInt(0, array.length);
+  let cnt = Number(array.length);
+  while (!viewed[index] && cnt >= 0) {
+    //O(1) search
+    index = randomInt(0, array.length);
+    cnt--;
+  }
+  return array[index].line;
 };
 
-module.exports.getLines = function() {
-    let index = randomInt(0, array.length);
-    favline.line = array[index].line;
-    favline.book = array[index].book;
-    favline.author = array[index].author;
-    return favline;
+module.exports.getLines = function () {
+  let index = randomInt(0, array.length);
+  favline.line = array[index].line;
+  favline.book = array[index].book;
+  favline.author = array[index].author;
+  return favline;
 };
