77575c2294f322134440714e52d6fd481092a7a1
DADDY-DOUBLESHOT <prathams1001@gmail.com>
2021-10-02 13:42:16 +0530

    hashmap with O(1) search time added

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 let array = require("./data/data.json");
-let viewed = []; //hashtable
+let viewed = new Array(array.length); //hashtable
 
 let favline = {};
 
 function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-module.exports.getRandomLine = function () {
-  let index = randomInt(0, array.length);
-  viewed[index] = 1;
-  return array[index].line;
+module.exports.getRandomLine = function() {
+    let index = randomInt(0, array.length);
+    viewed[index] = 1;
+    return array[index].line;
 };
 
-module.exports.getNewRandomLine = function () {
-  //new function to know which line was prevoiulsy generated and to not generate it again this time
+module.exports.getNewRandomLine = function() {
+    //new function to know which line was prevoiulsy generated and to not generate it again this time
 
-  let index = randomInt(0, array.length);
-  let cnt = Number(array.length);
-  while (!viewed.includes(index) && cnt >= 0) {
-    //might get executed in  1 iteration or might take log(n) iterations if used more in a day
-    index = randomInt(0, array.length);
-    cnt--;
-  }
-  return array[index].line;
+    let index = randomInt(0, array.length);
+    let cnt = Number(array.length);
+    while (!viewed[index] && cnt >= 0) { //O(1) search 
+        index = randomInt(0, array.length);
+        cnt--;
+    }
+    return array[index].line;
 };
 
-module.exports.getLines = function () {
-  let index = randomInt(0, array.length);
-  favline.line = array[index].line;
-  favline.book = array[index].book;
-  favline.author = array[index].author;
-  return favline;
+module.exports.getLines = function() {
+    let index = randomInt(0, array.length);
+    favline.line = array[index].line;
+    favline.book = array[index].book;
+    favline.author = array[index].author;
+    return favline;
 };
