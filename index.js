623c1918767ce93a3d74c5a502e453128db6fcdd
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-10-02 12:27:16 +0530

    Fix bug in index.js

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-module.exports.getRandomLine = function () {
-  let index = randomInt(0, array.length);
+module.exports.getRandomLine = function (type) {
+   let index = randomInt(0, array.length);
    switch(type){
     case 'isp' : 
+        index = randomInt(0, inspireArray.length);
         return inspireArray[index].line;
     default:
         return array[index].line;    
   }
 };
 
-module.exports.getLines = function () {
+module.exports.getLines = function (type) {
   let index = randomInt(0, array.length);
   switch(type){
     case "isp":
-        console.log(index);
+        index = randomInt(0, inspireArray.length);
         favline.line = inspireArray[index].line;
         favline.book = inspireArray[index].book;
         favline.author = inspireArray[index].author;
         return favline;
     default:
-        console.log(index);
         favline.line = array[index].line;
         favline.book = array[index].book;
         favline.author = array[index].author;    
