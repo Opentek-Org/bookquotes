4bfc85aa15b20357a3b4cf1e54f9ab8016810668
Saif <76686929+saif-malik-01@users.noreply.github.com>
2021-10-04 00:51:10 +0530

    Inspire quotes functionality and test added - #3 (#38)
    * Added inspire quotes & test
    
    * Update readme for type code list

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,7 +1,9 @@
 require("jest");
 
 const testGetLines = require("./getLines.test").default;
+const testGetLinesType = require("./getLinesType.test").default;
 const testGetRandomLines = require("./getRandomLine.test").default;
+const testGetRandomLinesType = require("./getRandomLineType.test").default;
 
 describe("testing all exposed functions", () => {
   test("getLines() function returns no error", () => {
@@ -13,6 +15,18 @@ describe("testing all exposed functions", () => {
   test("getRandomLines() function returns no error", () => {
     expect(() => {
       testGetRandomLines();
+    }).not.toThrow();
+  });
+ 
+  test("getRandomLines(<typeCode>) function returns no error", () => {
+    expect(() => {
+      testGetRandomLinesType('isp');
+    }).not.toThrow();
+  });
+
+test("getLines(<typeCode>) function returns no error", () => {
+    expect(() => {
+      testGetLinesType('isp');
     }).not.toThrow();
   });
 });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let array = require("./data/data.json");
+let inspireArray = require("./data/inspire.json");
 
 let favline = {};
 
@@ -6,15 +7,30 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-module.exports.getRandomLine = function () {
-  let index = randomInt(0, array.length);
-  return array[index].line;
+module.exports.getRandomLine = function (type) {
+   let index = randomInt(0, array.length);
+   switch(type){
+    case 'isp' : 
+        index = randomInt(0, inspireArray.length);
+        return inspireArray[index].line;
+    default:
+        return array[index].line;    
+  }
 };
 
-module.exports.getLines = function () {
+module.exports.getLines = function (type) {
   let index = randomInt(0, array.length);
-  favline.line = array[index].line;
-  favline.book = array[index].book;
-  favline.author = array[index].author;
-  return favline;
+  switch(type){
+    case "isp":
+        index = randomInt(0, inspireArray.length);
+        favline.line = inspireArray[index].line;
+        favline.book = inspireArray[index].book;
+        favline.author = inspireArray[index].author;
+        return favline;
+    default:
+        favline.line = array[index].line;
+        favline.book = array[index].book;
+        favline.author = array[index].author;    
+        return favline;
+  }
 };
